b6757d545ef4f7b726421b57757d148e16d94a33
smartwayswork <106230461+smartwayswork@users.noreply.github.com>
2025-06-21 19:39:17 +0100

    Updated MetricCard.tsx

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { LucideIcon, TrendingUp, TrendingDown } from 'lucide-react'
+import { DivideIcon as LucideIcon, TrendingUp, TrendingDown } from 'lucide-react'
 
 interface MetricCardProps {
   title: string
